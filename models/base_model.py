bbfe1ab562e463d39ba94820ddd7508ea0bf12b7
BABABUSOLA <queenbusola1@gmail.com>
2023-08-15 17:36:25 -0700

    udate basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -45,7 +45,7 @@ class BaseModel:
         Update the `updated_at` attribute with the current datetime.
         and call the save method on storage
         """
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
